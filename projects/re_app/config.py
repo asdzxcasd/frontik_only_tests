251a5e12ccda9bc2550f0135c9953dfd5475fce2
Andrew Sumin <sumin.andrew@gmail.com>
2016-01-16 18:03:06 +0300

    t

--- projects/re_app/config.py
+++ projects/re_app/config.py
@@ -5,7 +5,7 @@ import os
 from frontik.app import FileMappingDispatcher
 
 from . import pages
-from .pages import exception_on_init
+from .pages import exception_on_prepare
 from .pages import handler_404
 from .pages import id_param
 from .pages import simple
@@ -17,6 +17,5 @@ XSL_cache_limit = 1
 urls = [
     ('/id/(?P<id>[^/]+)', pages.id_param.Page),
     ('/not_simple', pages.simple.Page),
-    ('/fail_on_init', pages.exception_on_init.Page),
-    ('(?!/not_matching_regex)', FileMappingDispatcher(pages, handler_404=pages.handler_404.Page))
+    ('/fail_on_prepare', pages.exception_on_prepare.Page)
 ]
